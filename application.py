9eeadf57e95394219d6e6102a148d43c484c566f
bingdai9@gmail.com <bingdai9@gmail.com>
2024-01-26 20:50:23 -0800

    Refactor YouTube API key handling and update index.html template

--- application.py
+++ application.py
@@ -4,13 +4,13 @@ from services.youtube_service import YouTubeService
 import os
 
 # DEV: Load environment variables from .env file
-# from dotenv import load_dotenv
-# load_dotenv()
-# api_key = os.getenv('YT_API_KEY')
+from dotenv import load_dotenv
+load_dotenv()
+api_key = os.getenv('YT_API_KEY')
 
 
 # PROD: Get the YouTube API key from the environment properties
-api_key = os.getenv('YT_API_KEY')
+# api_key = os.getenv('YT_API_KEY')
 
 if not api_key:
     raise RuntimeError("YT_API_KEY not set")
@@ -24,10 +24,15 @@ def index():
         'UUGaVdbSav8xWuFWTadK6loA', 
         'UUcefcZRL2oaA_uBNeo5UOWg'
         ]  # Add your playlist IDs here
-    videos = {}
+    playlists = {}
     for playlist_id in playlist_ids:
-        videos[playlist_id] = youtube_service.get_latest_videos(playlist_id)
-    return render_template('index.html', videos=videos)
+        videos = youtube_service.get_latest_videos(playlist_id)
+        if videos['items']:
+            channel_title = videos['items'][0]['snippet']['channelTitle']
+            channel_id = videos['items'][0]['snippet']['channelId']
+            channel_url = f"https://www.youtube.com/channel/{channel_id}"
+            playlists[channel_title] = {'videos': videos, 'url': channel_url}
+    return render_template('index.html', playlists=playlists)
 
 if __name__ == '__main__':
     application.run(debug=True)
