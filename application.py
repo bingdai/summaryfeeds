6fe332595478b2d9eeaa1aed073604bbea9cf8f5
bingdai9@gmail.com <bingdai9@gmail.com>
2024-01-26 18:09:12 -0800

    Update YouTube API key handling

--- application.py
+++ application.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template
 from services.youtube_service import YouTubeService
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import os
 
+# DEV
 # Load environment variables from .env file
-load_dotenv()
+#load_dotenv()
 
 # Get the API key from the environment variable
+#api_key = os.getenv('YT_API_KEY')
+
+
+# Get the YouTube API key from the environment variable
 api_key = os.getenv('YT_API_KEY')
+
+if not api_key:
+    raise RuntimeError("YT_API_KEY not set")
 
 app = Flask(__name__)
 youtube_service = YouTubeService(api_key=api_key)
