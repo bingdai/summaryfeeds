40f17b75056c1ad90965c32d91c1d74ba251e902
bingdai9@gmail.com <bingdai9@gmail.com>
2024-01-26 17:37:45 -0800

    Add Flask app with YouTube integration

--- application.py
+++ application.py
@@ -1,8 +1,27 @@
+from flask import Flask, render_template
+from services.youtube_service import YouTubeService
 from dotenv import load_dotenv
 import os
 
+# Load environment variables from .env file
 load_dotenv()
 
 # Get the API key from the environment variable
 api_key = os.getenv('YT_API_KEY')
-print(api_key)
+
+app = Flask(__name__)
+youtube_service = YouTubeService(api_key=api_key)
+
+@app.route('/')
+def index():
+    playlist_ids = [
+        'UUGaVdbSav8xWuFWTadK6loA', 
+        'UUcefcZRL2oaA_uBNeo5UOWg'
+        ]  # Add your playlist IDs here
+    videos = {}
+    for playlist_id in playlist_ids:
+        videos[playlist_id] = youtube_service.get_latest_videos(playlist_id)
+    return render_template('index.html', videos=videos)
+
+if __name__ == '__main__':
+    app.run(debug=True)
