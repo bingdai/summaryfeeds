e90d3e6a7e0387a12bc516bd890c1ca05795e667
bingdai9@gmail.com <bingdai9@gmail.com>
2024-01-29 20:44:04 -0800

    Add featured flag to Channel model and update index route

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ from email.mime import application
 from flask import Flask, render_template
 from services.youtube_service import YouTubeService
 from database.connection import init_db
+from database.models.channel import Channel
 from dotenv import load_dotenv
 import os
 
@@ -13,7 +14,7 @@ application = Flask(__name__)
 
 # Initialize the YouTube service
 api_key = os.getenv('YT_API_KEY')
-print(f'YouTube API key: {api_key}')
+
 if not api_key:
     raise RuntimeError("YT_API_KEY not set")
 youtube_service = YouTubeService(api_key=api_key)
@@ -23,20 +24,17 @@ db = init_db(application)
 
 @application.route('/')
 def index():
-    playlist_ids = [
-        'UUGaVdbSav8xWuFWTadK6loA', 
-        'UUcefcZRL2oaA_uBNeo5UOWg',
-        'UUNJ1Ymd5yFuUPtn21xtRbbw',
-        'UUFn4S3OexFT9YhxJ8GWdUYQ'
-        ]  # Add your playlist IDs here
+    # Query featured channels
+    featured_channels = Channel.query.filter_by(featured=True).all()
     playlists = {}
-    for playlist_id in playlist_ids:
+    
+    for channel in featured_channels:
+        playlist_id = 'UU' + channel.channel_id[2:]  # Convert channel_id to playlist_id
         videos = youtube_service.get_latest_videos(playlist_id)
         if videos['items']:
-            channel_title = videos['items'][0]['snippet']['channelTitle']
-            channel_id = videos['items'][0]['snippet']['channelId']
-            channel_url = f"https://www.youtube.com/channel/{channel_id}"
-            playlists[channel_title] = {'videos': videos, 'url': channel_url}
+            channel_url = f"https://www.youtube.com/channel/{channel.channel_id}"
+            playlists[channel.channel_title] = {'videos': videos, 'url': channel_url}
+
     return render_template('index.html', playlists=playlists)
 
 if __name__ == '__main__':
--- database/models/channel.py
+++ database/models/channel.py
@@ -5,6 +5,7 @@ class Channel(db.Model):
 
     channel_id = db.Column(db.String(255), primary_key=True)
     channel_title = db.Column(db.String(255), nullable=False)
+    featured = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return f'<Channel {self.channel_title}>'
