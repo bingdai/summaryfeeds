48c3efadda870d7f60d27700a9488b7198ebe237
bingdai9@gmail.com <bingdai9@gmail.com>
2024-01-29 17:24:45 -0800

    Add Video model to database

---
+++ database/models/video.py
@@ -0,0 +1,20 @@
+from database.connection import db
+
+class Video(db.Model):
+    __tablename__ = 'videos'
+
+    id = db.Column(db.Integer, primary_key=True)
+    video_id = db.Column(db.String(255), unique=True, nullable=False)
+    published_at = db.Column(db.DateTime, nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+    channel_id = db.Column(db.String(255), db.ForeignKey('channels.channel_id'))
+    default_language = db.Column(db.String(50))
+    default_audio_language = db.Column(db.String(50))
+    duration = db.Column(db.String(50))
+    caption = db.Column(db.Boolean)
+    privacy_status = db.Column(db.String(50))
+    made_for_kids = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return f'<Video {self.title}>'
